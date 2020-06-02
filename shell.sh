8ae45ed05b5c0c12e02632a8519ad5439c0d3fda
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-06-02 10:14:46 +0530

    Shell script fix
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- shell.sh
+++ shell.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-gcc shell.c -L/usr/include -lreadline -o a.out
-gnome-terminal -e ./a.out
-rm ./a.out
+gcc shell.c -L/usr/include -lreadline -o a.out && gnome-terminal -e ./a.out && rm ./a.out
